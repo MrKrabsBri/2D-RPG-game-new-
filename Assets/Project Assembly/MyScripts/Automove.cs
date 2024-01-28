06535d74e399531280273d53f19b705dd7df9d6e
MrKrabsBri <julius.bri@outlook.com>
2024-01-28 15:56:23 +0200

    restructure: deleted unused scripts

--- Assets/Project Assembly/MyScripts/Automove.cs
+++ Assets/Project Assembly/MyScripts/Automove.cs
@@ -8,10 +8,10 @@ public class Automove : MonoBehaviourPun, IPunObservable {
     private float timer = 0f;
     private float moveSpeed = 3f; // Adjust the speed as needed
     private SpriteRenderer spriteRenderer;
-    private Vector3 smoothMove;
 
-    /*private Vector3 networkPosition;
-    private Quaternion networkRotation;*/
+    private Vector3 smoothMove;
+    private Vector3 networkPosition;
+    private Quaternion networkRotation;
 
     private void Start() {
         spriteRenderer = GetComponent<SpriteRenderer>();
@@ -47,13 +47,20 @@ public class Automove : MonoBehaviourPun, IPunObservable {
                 spriteRenderer.flipX = false;
             }
         }
-       /* else {
+        else {
+
+            // Smoothly interpolate towards the target position and rotation
             transform.position = Vector3.Lerp(transform.position, networkPosition, Time.deltaTime * 10);
             transform.rotation = Quaternion.Lerp(transform.rotation, networkRotation, Time.deltaTime * 10);
-        }*/
-       else {
-            SmoothMovement();
+
         }
+        /* else {
+             transform.position = Vector3.Lerp(transform.position, networkPosition, Time.deltaTime * 10);
+             transform.rotation = Quaternion.Lerp(transform.rotation, networkRotation, Time.deltaTime * 10);
+         }*/
+        /*       else {
+                    SmoothMovement();
+                }*/
 
     }
 
@@ -68,19 +75,26 @@ public class Automove : MonoBehaviourPun, IPunObservable {
             stream.SendNext(new Vector2(transform.position.x, transform.position.y));
             stream.SendNext(transform.rotation.z); // Send only the z-axis rotation for 2D
         }
-        else if (stream.IsReading){
+        else if (stream.IsReading) {
             // This is the remote client; receive the position and rotation
             Vector2 receivedPosition = (Vector2)stream.ReceiveNext();
             float receivedRotationZ = (float)stream.ReceiveNext();
 
             // Use the received data to update the object's state++++
-            transform.position = new Vector3(receivedPosition.x, receivedPosition.y, transform.position.z);
-            transform.rotation = Quaternion.Euler(0, 0, receivedRotationZ);
-            smoothMove = (Vector3)stream.ReceiveNext();
+            /*          transform.position = new Vector3(receivedPosition.x, receivedPosition.y, transform.position.z);
+                        transform.rotation = Quaternion.Euler(0, 0, receivedRotationZ);*/
+            //smoothMove = (Vector3)stream.ReceiveNext();
 
+            // Use Lerp for smoother movement
+            float lerpFactor = 0.1f; // Adjust this value based on desired smoothness
+
+            transform.position = Vector3.Lerp(transform.position, new Vector3(receivedPosition.x, receivedPosition.y, transform.position.z), lerpFactor);
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, receivedRotationZ), lerpFactor);
         }
-       // photonView.RPC("UpdateMonsterPosition", RpcTarget.AllBuffered, position, rotation);
 
     }
+    // photonView.RPC("UpdateMonsterPosition", RpcTarget.AllBuffered, position, rotation);
 
 }
+
+
